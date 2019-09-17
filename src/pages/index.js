6070d7256adb25e994c616a8ae79103cafbb1190
ninaristicc <ninaristic@hotmail.com>
2019-09-17 10:45:03 -0700

    content align

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,24 +20,27 @@ const IndexPage = () => (
     {/* slide 1 end */}
     {/* slide 2 start */}
     <section className="slide3" id="about">
+      <div className="content">
       <h2 className="ha2">About</h2>
     <p className="descr">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
     <div alt="TrueMoney" className="image" style={{ maxWidth: `540px`, marginBottom: `1.45rem` }}>
-    <ImageThree /> </div>
+    <ImageThree /> </div></div>
     </section>
     {/* slide 2 end */}
     {/* slide 3 start */}
     <section className="slide4" id="options">
+      <div className="content">
     <h2 className="ha2">Options</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
     <div alt="TrueMoney" className="image" style={{ maxWidth: `540px`, marginBottom: `1.45rem` }}>
-    <Image /> </div>
+    <Image /> </div></div>
     </section>
     {/* slide 3 end */}
     {/* slide 4 start */}
-    <section className="slide3" id="faqs">
+    <section className="slide3" id="faqs">      
+    <div className="content">
     <h2 className="ha2">FAQs</h2>
     <div className="descr">
     <h3>What is true money?</h3>
@@ -47,7 +50,7 @@ const IndexPage = () => (
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p></div>
     <div alt="TrueMoney" className="image" style={{ maxWidth: `540px`, marginBottom: `1.45rem` }}>
-    <ImageFour /> </div>
+    <ImageFour /> </div></div>
     </section>
     {/* slide 4 end */}
   </Layout>
